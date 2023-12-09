ca2199d705d0ff43b623c3b16a115266b6e502bc
okechukwu26 <priceblings@gmail.com>
2023-12-09 01:52:22 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from "./component/Navbar";
+import Header from "./component/Header";
+import Project from "./component/Project";
+import About from "./component/About";
+import Skills from "./component/Skills";
+import Works from "./component/Works";
+import Footer from "./component/Footer";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const toggleTheme = () => {
+    setDarkMode((prev) => !prev);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className={`w-full h-full min-h-[100vh] bg-white ${darkMode && "dark"}`}
+    >
+      <div className="w-full h-full min-h-[100vh] bg-white">
+        <div className="dark:bg-gradient-to-tr from-[#0235a3] via-[#030a1c] to-[#05174e]">
+          <Navbar
+            darkMode={darkMode}
+            isOpen={isOpen}
+            toggleMenu={toggleMenu}
+            toggleTheme={toggleTheme}
+          />
+
+          <section id="home" className="px-0 lg:px-5 2xl:px-40 py-10 lg:py-0">
+            <Header />
+          </section>
+        </div>
+
+        <section className="w-full bg-[#061130] py-20">
+          <Project />
+        </section>
+
+        <section
+          id="about"
+          className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#030a1c] to-[#05174e]"
         >
-          Learn React
-        </a>
-      </header>
+          <About />
+        </section>
+
+        <section
+          id="skills"
+          className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#05174e] to-[#030a1c]"
+        >
+          <Skills />
+        </section>
+
+        <section
+          id="projects"
+          className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#030a1c] to-[#05174e]"
+        >
+          <Works />
+        </section>
+
+      <div className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#05174e] to-[#030a1c]">
+          <Footer />
+      </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Mongodb, Nodejs,  Reactjs, Redux } from "../assets";
+import avatar from "../assets/avatar.jpeg"
+
+
+const Header = () => {
+  return (
+    <div className="w-full flex flex-col lg:flex-row px-8 md:px-10 gap-10 2xl:gap-20 py-20">
+      <div className="flex flex-col mb-20 lg:mb-0">
+        <span className="text-lg font-bold text-orange-700 ">
+          FULL-STACK ENGINEER
+        </span>
+        <div className="flex gap-4 items-center my-4">
+          <h1 className="text-gray-800 dark:text-neutral-100 text-5xl 2xl:text-7xl font-bold text-center">
+            I'm a
+          </h1>
+          <div className="flex items-center justify-center text-white shadow-lg bg-blue-800 dark:bg-[#224cff10] dark:text-[#224cff] rounded-full">
+            <p className="text-3xl 2xl:text-5xl font-bold py-2 px-5 pb-2 text-center">
+              Software Developer
+            </p>
+          </div>
+        </div>
+        <h1 className="text-5xl 2xl:text-7xl font-extrabold text-black dark:text-neutral-100 tracking-wider ">
+      Okechukwu
+        </h1>
+        <p className="text-md text-black dark:text-gray-100 mt-5 2xl:mt-10">
+          Full-Stack Web Developer with ability to learn and collaborate in
+          rapidly changing environments and compositions.
+        </p>
+        <div className="mt-10 flex gap-10">
+          <button className="bg-orange-700 text-lg text-white font-semibold rounded-md py-3 px-5">
+            Hire Me
+          </button>
+          <a href="#projects">
+            <button className="text-lg text-white font-semibold rounded-md py-3 px-5 bg-black dark:bg-[#224cff10]">
+              Projects
+            </button>
+          </a>
+        </div>
+      </div>
+    <div className="w-[320px] h-[320px] md:w-[600px] md:h-[600px] 2xl:w-[700px] 2xl:h-[700px] rounded-full border border-gray-600 dark:border-gray-200 relative md:nt-20 lg:mt-0 flex items-center justify-center">
+
+      <img src={avatar} className="w-full h-full absolute rounded-full object-cover " />
+      <div className="relative w-full h-full rounded-full">
+        <img src={Reactjs} className="absolute top-7 md:top-20 left-3 2xl:left-8 w-12 h-12 md:w-20 md:h-20"/>
+        <img src={Redux} className="absolute bottom-7 md:bottom-20 left-3 2xl:left-12 w-12 h-12 md:w-20 md:h-20"/>
+        <img src={Mongodb} className="absolute top-3 md:top-20 right-0 2xl:left-8 w-16 h-16 md:w-20 md:h-20"/>
+        <img src={Nodejs} className="absolute bottom-7 md:bottom-20 right-3 2xl:right-12 w-12 h-12 md:w-20 md:h-20"/>
+      </div>
+      
+
+    </div>
+    
+    </div>
+
+  );
+};
+
+export default Header;
